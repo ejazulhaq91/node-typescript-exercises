ddacefddad6e14416c54798d80a5168eba58dbf7
unknown <ejazulhaq91>
2024-03-30 00:10:12 +0500

    personal message 1

---
+++ 2.personal-message/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myName = "Ejaz Ulhaq";
+// console.log("Hello " + myName + ", Good luck for your typescript journey!")
+console.log("Hello ".concat(myName, ", Good luck for your typescript journey!"));
---
+++ 2.personal-message/app.ts
@@ -0,0 +1,6 @@
+let myName = "Ejaz Ulhaq"
+
+console.log(`Hello ${myName}, Good luck for your typescript journey!`)
+
+
+export{}
