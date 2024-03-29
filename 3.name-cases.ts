4519ca29d24c9afab5db326fc46bd3f234db62f0
ejazulhaq91 <ejazulhaq1@yahoo.com>
2024-03-30 01:06:57 +0500

    new

--- 3.name-cases.ts
+++ 3.name-cases.ts
@@ -1,12 +1,18 @@
+import {titleCase} from "title-case";
+
 let studentName = "Hamdaan khan"
 
 // Upper Case
 console.log(studentName.toUpperCase())
+
 // Lower Case
 console.log(studentName.toLowerCase())
+
 // Title Case
 const titledName = studentName.split(" ").map((name: string) => name[0].toUpperCase() + name.substr(1)).join(" ")
 console.log(titledName)
+
+
 
 
 
