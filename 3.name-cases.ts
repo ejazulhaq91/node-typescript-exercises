165416f566734b7e1d4d6931478d8832f33ac51b
ejazulhaq91 <ejazulhaq1@yahoo.com>
2024-03-30 00:50:07 +0500

    name case 1

---
+++ 3.name-cases.ts
@@ -0,0 +1,14 @@
+let studentName = "Hamdaan khan"
+
+// Upper Case
+console.log(studentName.toUpperCase())
+// Lower Case
+console.log(studentName.toLowerCase())
+// Title Case
+const titledName = studentName.split(" ").map((name: string) => name[0].toUpperCase() + name.substr(1)).join(" ")
+console.log(titledName)
+
+
+
+
+export{}
