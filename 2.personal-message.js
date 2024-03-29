3942f791f4d056f481bbcc6905946fb4ec57895a
unknown <ejazulhaq91>
2024-03-30 00:18:38 +0500

    environment setup

--- 2.personal-message.js
+++ 2.personal-message.js
@@ -1,5 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var myName = "Ejaz Ulhaq";
-// console.log("Hello " + myName + ", Good luck for your typescript journey!")
 console.log("Hello ".concat(myName, ", Good luck for your typescript journey!"));
